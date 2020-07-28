2ccded75dc5e2e1d30da70fd3f1669ce4aa19030
VShenderchuk <64258673+VShenderchuk@users.noreply.github.com>
2020-07-28 19:32:17 +0300

    Adds correct js

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,29 @@
 /*Fixed header*/
-    <script>
+    
+      /*
         const { height: pageHeaderHeight } = document
-        .querySelector(*.page-header*) 
+        .querySelector('*.page-header*') 
         .getBoundingClientRect();
         document.body.style.paddingTop = '${pageHeaderHeight}px';
-    </script>
-
+*/
 
 /*Script popup*/
  
- (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const refs = {
+  openModalBtn: document.querySelector('[data-open-modal]'),
+  closeModalBtn: document.querySelector('[data-close-modal]'),
+  backdrop: document.querySelector('[data-backdrop]'),
+};
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+refs.backdrop.addEventListener('click', logBackdropClick);
+
+function toggleModal() {
+  refs.backdrop.classList.toggle('is-hidden');
+}
+
+function logBackdropClick() {
+  console.log('Это клик в бекдроп');
+}
